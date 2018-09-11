f114ac67f77badbbf401fa94fb6b7e1d4fc679ef
hxc <249270087@qq.com>
2018-09-11 23:42:43 +0800

    增加部分代码

--- src/main/java/com/hxc/xsma/XsMaApplication.java
+++ src/main/java/com/hxc/xsma/XsMaApplication.java
@@ -7,6 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class XsMaApplication {
 
     public static void main(String[] args) {
+
+
         SpringApplication.run(XsMaApplication.class, args);
     }
 }
